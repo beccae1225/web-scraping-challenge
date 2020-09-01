f9820e71016cbc2f72b129834c6369149badf939
beccae1225@gmail.com <beccae1225@gmail.com>
2020-08-31 23:08:56 -0400

    final

--- Mission_To_Mars/scrape.py
+++ Mission_To_Mars/scrape.py
@@ -6,7 +6,24 @@ def init_browser():
     executable_path = {"executable_path": "/chromedriver.exe"}
     return Browser("chrome", **executable_path), headless=False)
 
-def scrape_info():
+def scrape():
+
+    browser = init_browser
+
+    dict = {}
+
+    news_title, news_p = news()
+
+    dict["title"] = news_title
+    dict["content"] = news_p
+    dict["main_image"] = image()
+    dict["facts"] = facts()
+    dict["hemisphere"] = hemisphere()
+
+    browser.quit
+    return dict
+
+def news():
     browser = init_browser()
 
     #Visit URL
@@ -19,16 +36,60 @@ def scrape_info():
     soup = bs(html, "html.parser")
 
     #news_title
-    news_titles = soup.find("div", class_="content_title")
+    news_title = soup.find("div", class_="content_title")
 
     #news paragraph
     news_p = soup.find("div", class_="article_teaser_body").text
 
+    broswer.quit()
+
+    return news_title, news_p
+
+def image():
+
+    browser = init_browser()
+
+    jpl_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+
+    browser.visit(jpl_url)
+
+    time.sleep(1)
+
+    browser.quit()
+
+def facts():
+
+    broswer = init_browser()
+
+    mars_facts_url = "https://space-facts.com/mars/"
+
+    tables = pd.read_html(mars_facts_url)
+    df = tables[0]
+
+    df.columns = ['Fact', 'Value']
+    df.set_index('Fact', inplace=True)
+    html_table = df.to_html()
+    html_table
+
+    browser.quit()
+
+    return html_table
+
+def hemisphere():
+
+    browser = init_browser()
+
+    jpl_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+
+    browser.visit(jpl_url)
+
+    time.sleep(1)
+
+    browser.quit()
 
     
 
 
 
-    browser.quit()
 
 
